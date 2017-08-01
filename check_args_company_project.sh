0f976987d2653049ffe475ecf5f6a8ee115b7a97
Vadim Shapka <vadim.shapka@strikersoft.com>
2017-08-01 13:35:44 +0300

    change check args

--- check_args_company_project.sh
+++ check_args_company_project.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
-echo "params $#"
+
+
+check_args_company_project () {
+	echo "params $#"
+	echo "check args $1 $2"
+
+	if [ -z "$1" ]
+	  then
+	    echo "No 1st argument COMPANY_NAME"
+	    echo "Usage: $0 COMPANY_NAME PROJECT_NAME"
+	    return false
+	fi
+
+	if [ -z "$2" ]
+	  then
+	    echo "No 2nd argument PROJECT_NAME"
+	    echo "Usage: $0 COMPANY_NAME PROJECT_NAME"
+	    return false
+	fi
+
+    return true
+}
+
+
 #if [ $# -ne 2 ]; then
 #   usage;
 #fi
@@ -8,16 +31,3 @@ echo "params $#"
 #    echo "No -t param for for 1st arg"
 #fi
 
-if [ -z "$1" ]
-  then
-    echo "No 1st argument COMPANY_NAME"
-    echo "Usage: $0 COMPANY_NAME PROJECT_NAME"
-    exit 1
-fi
-
-if [ -z "$2" ]
-  then
-    echo "No 2nd argument PROJECT_NAME"
-    echo "Usage: $0 COMPANY_NAME PROJECT_NAME"
-    exit 1
-fi
